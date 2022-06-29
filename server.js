00611a8e9ba3f810b9a5098b5537fb76f172919e
Brozovic <alejandro.brozovic@gmail.com>
2022-06-28 21:17:07 -0400

    Add the ability to upload files and store their data in MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
+require("dotenv").config()
+const multer = require("multer");
+const mongoose = require('mongoose');
 const express = require("express");
+const bcrypt = require("bcrypt");
+const File = require("./models/file.js")
 const app = express();
 
-app.set("view engine", "ejs");
+const upload = multer({dest: "uploads"});
 
-app.get("/", (req, res)=>{
+
+app.set("view engine", "ejs");
+mongoose.connect(process.env.DATABASE_URL);
+
+
+app.get("/", (req, res) => {
     res.render("index")
 });
-app.listen(3000);
+
+app.post("/upload", upload.single("file"), async (req, res) => {
+    const fileData = {
+        path: req.file.path,
+        originalName: req.file.originalname
+    }
+    if (req.body.password !== null && req.body.password !== "") {
+        fileData.password = await bcrypt.hash(req.body.password, 10);
+    }
+    const file = await File.create(fileData);
+
+    res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` })
+});
+app.listen(process.env.PORT);
